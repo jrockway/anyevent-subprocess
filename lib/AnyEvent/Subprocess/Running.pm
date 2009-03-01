f865d750239f1443f27e0af91de6cdfcdf5c9498
Jonathan Rockway <jon@jrock.us>
2009-02-28 20:25:04 -0600

    use Event::Join instead of our hand-rolled thing

--- lib/AnyEvent/Subprocess/Running.pm
+++ lib/AnyEvent/Subprocess/Running.pm
@@ -4,7 +4,7 @@ use MooseX::AttributeHelpers;
 use AnyEvent;
 use AnyEvent::Handle;
 use AnyEvent::Subprocess::Done;
-use List::Util qw(reduce);
+use Event::Join;
 
 with 'AnyEvent::Subprocess::Running::WithOutputCallbacks',
      'AnyEvent::Subprocess::Running::WithOutputAccumulator';
@@ -20,55 +20,6 @@ after child_pid => sub {
     $self->child_listener if defined $pid;
 };
 
-# this is updated by callbacks for EOF and child exit
-has 'completion_flags' => (
-    metaclass => 'Collection::Hash',
-    is        => 'ro',
-    isa       => 'HashRef',
-    default   => sub {+{}},
-    required  => 1,
-    provides  => {
-        set => 'set_completion_flag',
-    },
-);
-
-# every time a callback sets a completion flag, check if everthing is
-# done.  if so, send to the completion condvar
-after set_completion_flag => sub {
-    my $self = shift;
-    my $done = reduce { $a && $b }
-      (1,
-      (map { $self->completion_flags->{$_} } qw/stdout_handle stderr_handle/),
-      exists $self->completion_flags->{child});
-
-    if($done){
-        my $status = $self->completion_flags->{child};
-        $self->completion_condvar->send(
-            AnyEvent::Subprocess::Done->new(
-                exit_status => $status,
-                exit_value  => ($status >> 8),
-                exit_signal => ($status & 127),
-                dumped_core => ($status & 128),
-                stdout      => $self->stdout,
-                stderr      => $self->stderr,
-            ),
-        );
-
-        for my $name (qw/stdin_handle stdout_handle stderr_handle comm_handle/){
-            my $h = $self->$name;
-            $h->destroy;
-        }
-    }
-};
-
-has 'completion_condvar' => (
-    is      => 'ro',
-    isa     => 'AnyEvent::CondVar',
-    default => sub {
-        AnyEvent->condvar,
-    },
-);
-
 has 'child_listener' => (
     is      => 'ro',
     lazy    => 1,
@@ -78,11 +29,51 @@ has 'child_listener' => (
             pid => $self->child_pid,
             cb => sub {
                 my ($pid, $status) = @_;
-                $self->set_completion_flag( child => $status );
+                $self->child_event_joiner->send_event( child => $status );
             },
         );
         return $child_listener;
     }
+);
+
+has 'completion_condvar' => (
+    is      => 'ro',
+    isa     => 'AnyEvent::CondVar',
+    default => sub {
+        AnyEvent->condvar,
+    },
+);
+
+has 'child_event_joiner' => (
+    is       => 'ro',
+    isa      => 'Event::Join',
+    required => 1,
+    default  => sub {
+        my $self = shift;
+        return Event::Join->new(
+            events        => [qw/stdout_handle stderr_handle child/],
+            on_completion => sub {
+                my $events = shift;
+                my $status = $events->{child};
+
+                $self->completion_condvar->send(
+                    AnyEvent::Subprocess::Done->new(
+                        exit_status => $status,
+                        exit_value  => ($status >> 8),
+                        exit_signal => ($status & 127),
+                        dumped_core => ($status & 128),
+                        stdout      => $self->stdout,
+                        stderr      => $self->stderr,
+                    ),
+                );
+
+                for my $name (qw/stdin_handle stdout_handle stderr_handle comm_handle/){
+                    my $h = $self->$name;
+                    $h->destroy;
+                }
+            }
+        );
+    },
 );
 
 has [qw/stdout_handle stderr_handle stdin_handle comm_handle/] => (
@@ -105,9 +96,10 @@ sub _setup_handle {
         $self->$handle_name->push_read(line => $reader);
     }
 
-    $self->$handle_name->eof_condvar->cb(sub {
-      $self->set_completion_flag($handle_name, 1);
-    });
+    $self->$handle_name->eof_condvar->cb(
+        $self->child_event_joiner->event_sender_for($handle_name),
+    ) if $method_name;
+    # for some reason, we never get an EOF event for the comm_handle
 }
 
 sub BUILD {
