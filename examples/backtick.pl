8dc0c8ffc9068a7fb7a9da737c625e4c2b80ded1
Jonathan Rockway <jon@jrock.us>
2009-10-19 06:03:22 -0500

    commit example

---
+++ examples/backtick.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature ':5.10';
+
+use AnyEvent::Subprocess::Easy qw(qx_nonblock);
+
+# my $result = `ls -l`;
+my $result = qx_nonblock(qw/ls -l/)->recv; # your program can handle other
+                                           # events while you are waiting
+
+print "Output of ls:\n $result";
